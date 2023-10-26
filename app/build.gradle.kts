f310fa3e8aa23a2cd469b8468e4b4715eaf0c740
Special-N9NE <abigdeli42@gmail.com>
2023-10-26 23:33:21 +0330

    bottom navigation

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,12 +5,12 @@ plugins {
 
 android {
     namespace = "org.n9ne.h2ohealthy"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "org.n9ne.h2ohealthy"
         minSdk = 24
-        targetSdk = 33
+        targetSdk = 34
         versionCode = 1
         versionName = "1.0"
 
@@ -41,12 +41,12 @@ android {
 
 dependencies {
 
-    implementation("androidx.navigation:navigation-fragment-ktx:2.1.0-beta01")
-    implementation("androidx.navigation:navigation-ui-ktx:2.1.0-beta01")
+    implementation("androidx.navigation:navigation-fragment-ktx:2.7.4")
+    implementation("androidx.navigation:navigation-ui-ktx:2.7.4")
 
-    implementation("androidx.core:core-ktx:1.9.0")
+    implementation("androidx.core:core-ktx:1.12.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
-    implementation("com.google.android.material:material:1.9.0")
+    implementation("com.google.android.material:material:1.10.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
     implementation("com.google.android.gms:play-services-base:18.2.0")
     testImplementation("junit:junit:4.13.2")
--- app/src/main/java/org/n9ne/h2ohealthy/MainActivity.kt
+++ app/src/main/java/org/n9ne/h2ohealthy/MainActivity.kt
@@ -1,11 +1,15 @@
 package org.n9ne.h2ohealthy
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import org.n9ne.h2ohealthy.databinding.ActivityMainBinding
+
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var b: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        b = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(b.root)
     }
 }
